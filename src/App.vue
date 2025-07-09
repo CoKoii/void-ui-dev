9fa5bfe799774f213bb85c475b2839cc9d6ebc33
CaoKai <2655257336@qq.com>
2025-07-09 21:08:46 +0800

    Button组件

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import VButton from './components/Button/Button.vue'
+defineOptions({
+  name: 'App',
+})
+</script>
+
+<template>
+  <div class="App">
+    <v-button type="primary" plain disabled>Click Me</v-button>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { ButtonProps } from './types'
+defineProps<ButtonProps>()
+defineOptions({
+  name: 'VButton',
+})
+</script>
+
+<template>
+  <button
+    class="v-button"
+    :class="{
+      [`v-button--${type}`]: type,
+      [`v-button--${size}`]: size,
+      'is-plain': plain,
+      'is-circle': circle,
+      'is-round': round,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped></style>
