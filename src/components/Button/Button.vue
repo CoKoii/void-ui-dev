548448a97a2ab29bb0ab5b7f86ffef6a7af18c73
CK <2655257336@qq.com>
2025-07-12 19:36:11 +0800

    更新按钮组件，重构按钮模板结构，优化样式和交互效果

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -24,32 +24,37 @@ defineExpose({
 </script>
 
 <template>
-  <button
-    ref="_ref"
-    class="v-button"
-    @click="ButtonClick"
-    :class="{
-      [`v-button--${type}`]: type,
-      [`v-button--${size}`]: size,
-      'is-plain': plain,
-      'is-circle': circle,
-      'is-round': round,
-      'is-disabled': disabled,
-    }"
-    :disabled="disabled"
-    :type="nativeType"
-    :autofocus="autofocus"
-    :style="{
-      backgroundColor: type ? `color-mix(in srgb, ${computedColor} 95%, black)` : 'var(--gray-1)',
-      boxShadow: type
-        ? `color-mix(in srgb, ${computedColor} 10%, transparent) 0px 2px 0px 0px`
-        : `color-mix(in srgb, currentColor 3%, transparent) 0px 2px 0px 0px`,
-      color: type
-        ? 'var(--text-color-light)'
-        : `color-mix(in srgb, ${computedColor} 70%, transparent)`,
-      borderColor: type ? `color-mix(in srgb, ${computedColor} 50%, transparent)` : 'currentColor',
-    }"
-  >
+  <div class="v-button-container">
+    <button
+      ref="_ref"
+      class="v-button"
+      @click="ButtonClick"
+      :class="{
+        [`v-button--${type}`]: type,
+        [`v-button--${size}`]: size,
+        'is-plain': plain,
+        'is-circle': circle,
+        'is-round': round,
+        'is-disabled': disabled,
+      }"
+      :disabled="disabled"
+      :type="nativeType"
+      :autofocus="autofocus"
+      :style="{
+        backgroundColor: type ? `color-mix(in srgb, ${computedColor} 95%, black)` : 'var(--gray-1)',
+        boxShadow: type
+          ? `color-mix(in srgb, ${computedColor} 10%, transparent) 0px 2px 0px 0px`
+          : `color-mix(in srgb, currentColor 3%, transparent) 0px 2px 0px 0px`,
+        color: type
+          ? 'var(--text-color-light)'
+          : `color-mix(in srgb, ${computedColor} 70%, transparent)`,
+        borderColor: type
+          ? `color-mix(in srgb, ${computedColor} 50%, transparent)`
+          : 'currentColor',
+      }"
+    >
+      <slot></slot>
+    </button>
     <span
       v-for="ripple in ripples"
       :key="ripple.id"
@@ -60,8 +65,7 @@ defineExpose({
           : `color-mix(in srgb, ${computedColor || 'var(--gray-6)'} 20%, transparent)`,
       }"
     ></span>
-    <slot></slot>
-  </button>
+  </div>
 </template>
 
 <style scoped>
