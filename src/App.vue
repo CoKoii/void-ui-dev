7339b67dff525d69e9a3162b559c3ce9dc98f9f9
CK <2655257336@qq.com>
2025-08-27 13:48:24 +0800

    添加主题切换功能，更新样式和依赖项

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,27 @@
 defineOptions({
   name: 'App',
 })
+import VThemeToggle from './components/ThemeToggle/index.vue'
+const toggleTheme = () => {
+  const currentTheme = document.documentElement.getAttribute('data-theme')
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark'
+  document.documentElement.setAttribute('data-theme', newTheme)
+}
 </script>
 
 <template>
-  <div class="App"></div>
+  <div class="App">
+    <VThemeToggle :onToggle="toggleTheme">
+      <button>切换日夜间</button>
+    </VThemeToggle>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.App {
+  width: 100vw;
+  height: 100vh;
+  overflow: auto;
+  background-color: var(--gray-1);
+}
+</style>
---
+++ src/components/ThemeToggle/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { VThemeToggleProps } from './types'
+
+const props = withDefaults(defineProps<VThemeToggleProps>(), {})
+const toggleTheme = () => {
+  if (props.onToggle) {
+    props.onToggle()
+  }
+}
+defineOptions({
+  name: 'VThemeToggle',
+})
+</script>
+
+<template>
+  <button class="VThemeToggle" @click="toggleTheme">
+    <slot />
+  </button>
+</template>
+
+<style scoped lang="scss">
+@use './style.scss';
+</style>
