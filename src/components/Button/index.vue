df9b8160acf2815e2f35342cb7b91f3910fe3ecb
CK <2655257336@qq.com>
2025-08-27 17:48:07 +0800

    添加 VButton 组件的初始实现

---
+++ src/components/Button/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'VButton',
+})
+</script>
+
+<template>
+  <button style="background-color: var(--blue-1)">v-button</button>
+</template>
+
+<style scoped lang="scss"></style>
