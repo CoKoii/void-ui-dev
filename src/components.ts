62d740d5e196f2fd0f1c8b7a482dc01b67bb5ea7
CK <2655257336@qq.com>
2025-08-27 18:36:41 +0800

    更新版本号至 1.0.6，移除 VButton 组件及相关导入

--- src/components.ts
+++ src/components.ts
@@ -1,5 +1,4 @@
 // 自动生成，请勿手动修改
-export { default as VButton } from './components/Button'
 export { default as VThemeToggle } from './components/ThemeToggle'
 
 // 导出类型
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import type { App } from 'vue'
 
 // 自动生成导入
-import VButton from './components/Button/index.vue'
 import VThemeToggle from './components/ThemeToggle/index.vue'
 
 import './styles/main.scss'
 
-const components = [VButton, VThemeToggle] as const
+const components = [VThemeToggle] as const
 
 const install = (app: App): void => {
   components.forEach((component) => {
@@ -16,11 +15,11 @@ const install = (app: App): void => {
   })
 }
 
-const version = '1.0.5'
+const version = '1.0.6'
 
 const VoidDesignVue = { install, version }
 
-export { VButton, VThemeToggle }
+export { VThemeToggle }
 export * from './components'
 export { install, version }
 export default VoidDesignVue
