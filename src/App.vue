a9ce9e1621b13c4837d2d19566c2ba4f6bcad1c4
CK <2655257336@qq.com>
2025-08-28 21:06:59 +0800

    添加 Ant Design Vue 依赖，重构 VCard 组件，优化样式和布局

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,17 @@ defineOptions({
   name: 'App',
 })
 import VThemeToggle from './components/ThemeToggle/index.vue'
-import VIcon from './components/Icon/index.vue'
-import { faSun } from '@fortawesome/free-solid-svg-icons'
+import VCard from './components/Card/index.vue'
 </script>
 
 <template>
   <div class="App">
-    <VThemeToggle> </VThemeToggle>
-    <VThemeToggle> </VThemeToggle>
-    <span>666</span>
-    <VIcon :icon="faSun" :color="'var(--v-gray-9)'" />
+    <VThemeToggle class="changeTheme" />
+    <VCard style="width: 50vw">
+      <template #title>基本使用</template>
+      Vben Admin 是一个基于 Vue3.0、Vite、 TypeScript
+      的后台解决方案，目标是为开发中大型项目提供开箱即用的解决方案。包括二次封装组件、utils、hooks、动态菜单、权限校验、多主题配置、按钮级别权限控制等功能。项目会使用前端较新的技术栈，可以作为项目的启动模版，以帮助你快速搭建企业级中后台产品原型。也可以作为一个示例，用于学习
+    </VCard>
   </div>
 </template>
 
@@ -21,17 +22,16 @@ import { faSun } from '@fortawesome/free-solid-svg-icons'
   width: 100vw;
   height: 100vh;
   overflow: auto;
-  background-color: var(--v-gray-1);
+  background-color: var(--v-page-bg-color);
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   gap: 24px;
 }
-.icon {
-  padding: 13px;
-  background-color: transparent;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+.changeTheme {
+  position: absolute;
+  right: 30px;
+  top: 30px;
 }
 </style>
---
+++ src/components/Card/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { CardProps } from 'ant-design-vue'
+
+const props = withDefaults(defineProps<CardProps>(), {
+  bordered: true,
+})
+const slots = defineSlots()
+defineOptions({
+  name: 'VCard',
+})
+</script>
+
+<template>
+  <div class="VCard" :class="{ bordered: props.bordered }">
+    <div class="title" v-if="slots.title || slots.extra || props.title">
+      <div class="left">
+        <slot name="title">{{ props.title }}</slot>
+      </div>
+      <div class="right">
+        <slot name="extra"></slot>
+      </div>
+    </div>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use './index.scss';
+</style>
