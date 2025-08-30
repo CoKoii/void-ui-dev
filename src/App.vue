8c2f3e21612d3183b4628c1ca6d0f831d408a672
CK <2655257336@qq.com>
2025-08-30 15:04:39 +0800

    优化 VCode 组件，新增文件路径显示功能；调整路径文本样式，修改为加粗并禁用用户选择

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@ import { faDownload } from '@fortawesome/free-solid-svg-icons'
       :extra="{ copy: true, download: false, showLang: true }"
       lang="js"
       style="width: 600px"
+      path="main.ts"
     >
       {{
         "import type { App } from 'vue'\nimport { version } from './version'\n\n// 组件导入\nimport VCard from './components/Card/index.vue'\nimport VCode from './components/Code/index.vue'\nimport VIcon from './components/Icon/index.vue'\nimport VThemeToggle from './components/ThemeToggle/index.vue'\n\n// 样式导入\nimport './styles/main.scss'\n\n// 组件列表\nconst components = [VCard, VCode, VIcon, VThemeToggle] as const\n\n// 安装函数\nconst install = (app: App): void => {\n  components.forEach((component) => {\n    if (component.name) {\n      app.component(component.name, component)\n    }\n  })\n}\n\n// 默认导出\nconst VoidDesignVue = { install, version }\n\n// 组件导出\nexport { VCard, VCode, VIcon, VThemeToggle }\n\n// 类型导出\nexport * from './components'\n\n// 插件导出\nexport { install, version }\nexport default VoidDesignVue"
